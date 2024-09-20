75c7f708c364a1c96066c355661242177d4eca65
mauraisa <mauraisa@uw.edu>
2024-09-20 13:19:52 -0700

    Add --stub and --branch parameters to run_test_workflows

--- run_test_workflows.sh
+++ run_test_workflows.sh
@@ -1,28 +1,38 @@
 
-NEXTFLOW_RUN='nextflow run /home/ajm/code/nf-teirex-dia/main.nf -profile standard -c pipeline.config'
+REPO='/home/ajm/code/nf-teirex-dia/main.nf'
 RESUME='-resume'
 STUB=''
+BRANCH=''
 
-for i in "$@"
-do
-case $i in
+while ! [[ -z "$1" ]] ; do
+case $1 in
     -f|--noResume)
         RESUME=''
     ;;
     --stub)
         STUB='-stub'
     ;;
+    --repo)
+        shift
+        REPO="$1"
+    ;;
+    --branch)
+        shift
+        BRANCH="-r $1"
+    ;;
     -h|--help)
-        echo "run_test_workflows.sh [--stub] [--noResume]"
+        echo "run_test_workflows.sh [--stub] [--noResume] [--repo <repository>] [--branch <branch>]"
+        exit 0
     ;;
     *)
-        echo "Unknown option: \'$i\'"
+        echo "Unknown option: \'$1\'"
         exit 1
     ;;
 esac
+shift
 done
 
-NEXTFLOW_RUN="${NEXTFLOW_RUN} $RESUME $STUB"
+NEXTFLOW_RUN="nextflow run ${REPO} ${BRANCH} -profile standard -c pipeline.config ${RESUME} ${STUB}"
 
 TEST_DIRS=('local_mzML_diann' 'local_mzML_enc' 'local_mzML_enc_wide_only' 'pdc_diann' 'pdc_encyclopedia')
 
