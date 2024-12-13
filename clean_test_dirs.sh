76b0f5affc1c164532c8c7f20d9247f3600681cb
mauraisa <mauraisa@uw.edu>
2024-12-13 10:47:24 -0800

    Add dirs to clean_test_dirs

--- clean_test_dirs.sh
+++ clean_test_dirs.sh
@@ -1,5 +1,5 @@
 
-TEST_DIRS=('local_mzML_diann' 'local_mzML_enc' 'local_mzML_enc_wide_only' 'pdc_diann' 'pdc_encyclopedia')
+TEST_DIRS=('local_mzML_diann' 'local_mzML_enc' 'local_mzML_enc_wide_only' 'pdc_diann' 'pdc_encyclopedia' 'panorama_raw_diann' 'panorama_mzML_diann')
 
 for d in ${TEST_DIRS[@]} ; do
     pushd $d
